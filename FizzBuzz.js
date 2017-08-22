6878eeed98ddcb45be9e2e595327437deaf1411c
JamesDimi <ddimitrakop0@gmail.com>
2017-08-22 13:56:11 +0300

    Created ESlint configuration

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,37 +1,37 @@
 // My solution
 function fizzBuzzPartA()
 {
-    for(var x = 1; x <= 100; x++ ) {
+	for(var x = 1; x <= 100; x++ ) {
 
-        if( x % 3 == 0) {
-            console.log("Fizz");
-        }
-        else if( x % 5 == 0) {
-            console.log("Buzz");
-        }
-        else {
-            console.log(x);
-        }
-    }
+		if( x % 3 == 0) {
+			console.log("Fizz");
+		}
+		else if( x % 5 == 0) {
+			console.log("Buzz");
+		}
+		else {
+			console.log(x);
+		}
+	}
 }
 
-function fizzBuzzPartB() 
+function fizzBuzzPartB()
 {
-    for(var x = 1; x <= 100; x++ ) {
-        
-        if( (x % 3 == 0) && (x % 5 == 0) ) {
-            console.log("FizzBuzz");
-        }
-        else if( x % 3 == 0) {
-            console.log("Fizz");
-        }
-        else if( x % 5 == 0) {
-            console.log("Buzz");
-        }
-        else {
-            console.log(x);
-        }
-    }
+	for(var x = 1; x <= 100; x++ ) {
+
+		if( (x % 3 == 0) && (x % 5 == 0) ) {
+			console.log("FizzBuzz");
+		}
+		else if( x % 3 == 0) {
+			console.log("Fizz");
+		}
+		else if( x % 5 == 0) {
+			console.log("Buzz");
+		}
+		else {
+			console.log(x);
+		}
+	}
 }
 
 console.log("FizzBuzzPartA");
@@ -40,20 +40,20 @@ console.log("\nFizzBuzzPartPartB");
 fizzBuzzPartB();
 
 // BookSolution
-function bookfunction() 
+function bookfunction()
 {
-    for( var n = 1; n <= 100; n++) {
-        
-        var output = "";
-            
-        if( n % 3 == 0) {
-            output += "Fizz";
-        }
-        if(n % 5 == 0) {
-            output += "Buzz";
-        }
-        console.log( output || n);
-    }
+	for( var n = 1; n <= 100; n++) {
+
+		var output = "";
+
+		if( n % 3 == 0) {
+			output += "Fizz";
+		}
+		if(n % 5 == 0) {
+			output += "Buzz";
+		}
+		console.log( output || n);
+	}
 }
 
 console.log("\nBook Solution:");
