3af5a98a4c1d1fbb585afc476cf18e63a50ab723
JamesDimi <ddimitrakop0@gmail.com>
2017-08-25 15:22:05 +0300

    defined what i must do in the historical exercise

---
+++ chapter5/Historical_life_expectancy.js
@@ -0,0 +1,19 @@
+// Compute and output the average age of the people in the ancestry data set per century.
+// A person is assigned to a century by taking their year of death,
+// dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100)
+
+function average()
+{
+  function plus(a, b){
+    return a + b;
+  }
+}
+
+// Your code here.
+
+// → 16: 43.5
+//   17: 51.2
+//   18: 52.8
+//   19: 54.8
+//   20: 84.7
+//   21: 94
